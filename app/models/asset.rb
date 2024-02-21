ebe2a2983ddebbcb00f6e16f86bdf41345912622
zyx1121 <yongxiang.zhan@outlook.com>
2024-02-21 06:09:27 +0000

    🪛 修改資產模型

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,8 +1,4 @@
 class Asset < ActiveRecord::Base
   validates :name, presence: true
   validates :status, inclusion: { in: %w[Available CheckedOut UnderMaintenance] }
-
-  def is_available?
-    status == 'Available'
-  end
 end
--- db/migrate/20240206084806_create_assets.rb
+++ db/migrate/20240206084806_create_assets.rb
@@ -2,6 +2,7 @@ class CreateAssets < ActiveRecord::Migration[6.1]
   def change
     create_table :assets do |t|
       t.string :name
+      t.text :description
       t.string :status
     end
   end
